d9fb5a38d28945dda67eb559af000a374e22ba45
Cleiton Monteiro <cleitonmontsilva@gmail.com>
2019-04-27 20:05:33 -0300

    add atribuicao de valores

--- estrutura-basica/variaveis_tipos_basicos .c
+++ estrutura-basica/variaveis_tipos_basicos .c
@@ -7,7 +7,21 @@ int main() {
     char    k; // variavel para caracteres
     float   g; // variavel para valores reais
     double  i; // variavel para valores reais, porem com dupla precisao nas casas decimais
-    
 
+    a = 10;  // atribuindo o valor 10 a a varialvel a
+
+    /**
+     * Atribuindo o caractere 'F' a varialvel k.
+     * Obs : perceba as aspas simples.
+     * Obs : o tipo char é para apenas um caracactere.
+    */
+    k = 'F';
+
+    /**
+     * Atribuindo valores reais as variaveis g e i
+     * Obs : perceba que usamos '.' ao inver de ',' para numeros reais.
+    */
+    g = 12.1;
+    i = 12.1;
     return 0;
 }
